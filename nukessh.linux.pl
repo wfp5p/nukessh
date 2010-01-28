0dd8b3a00b1652c1cd1161edefa7996efd69b17b
Bill Pemberton <wfp5p@virginia.edu>
2010-01-28 16:54:09 -0500

    Remove trailing whitespace

--- nukessh.linux.pl
+++ nukessh.linux.pl
@@ -16,7 +16,7 @@ $NUKEDBM='/etc/uva/nukessh/nukedbm';
 $EXPIRECYCLE = 3600;
 $EXPIRE=43200; # how long a host stays blocked
 $DECAY=10;
-$THRESHOLD=100; 
+$THRESHOLD=100;
 
 GetOptions('debug',\$DEBUGOPT);
 
@@ -53,98 +53,98 @@ sub flip_debug
    $DEBUG = (! $DEBUG );
 }
 
-sub logIt 
+sub logIt
 {
   my ($message,$force) = @_;
-  
+
   return if ( (! $DEBUG) && ( ! $force) );
-  
+
   my $now_string = strftime "%b %e %H:%M:%S %Y", localtime();
-  
+
   print $LOG "$now_string $message\n";
 }
 
 sub blockHost
-{   
+{
     my $ip = shift;
-    
+
     system "/sbin/iptables -A nukessh -s $ip -j DROP";
-    
+
     $DBM{$ip} = $NOW + $EXPIRE;
-    
+
     delete $ipcount{$ip};
-    
-    logIt("blocking $ip",1); 
-    
-}  
+
+    logIt("blocking $ip",1);
+
+}
 
 sub unblockHost
-{   
+{
     my $ip = shift;
-    
+
     system "/sbin/iptables -D nukessh -s $ip -j DROP";
-    
+
     delete $DBM{$ip};
-    
-    logIt("unblocking $ip",1); 
-    
-}  
+
+    logIt("unblocking $ip",1);
+
+}
 
 sub dumpTable
-{ 
-   
+{
+
    my $saveDebug = $DEBUG;
    my $entry;
-   
+
    $DEBUG = 1;
-    
+
    logIt ("Dumping ipcounts table:");
-    
+
     while (my ($key,$val) = each %ipcount)
     {
 	logIt ("  $key $val");
     }
-   
+
    logIt ("Dumping DBM file:");
-   
+
    while  ( my ($key,$val) = each %DBM)
    {
       logIt ("  $key $val");
    }
-   
+
    undef $DUMPTABLE;
    $DEBUG = saveDebug;
-}   
+}
 
-   
-  
+
+
 sub expireHosts
 {
    my $entry;
-   
+
    logIt("Doing expire.....");
-    
+
    while  ( my ($key,$val) = each %ipcount)
    {
        if ($val <= $DECAY) { delete $ipcount{$key}; }
        else { $ipcount{$key} -= $DECAY; }
-   }	
-   
+   }
+
    while  ( my ($key,$val) = each %DBM)
    {
        if ($DBM{$key} < $NOW)
        {
            unblockHost($key);
-       	   
+
        }
 
    }
-   
+
    $nextExpireRun = $NOW + $EXPIRECYCLE;
-   
+
    logIt("Expire done, next expire at $nextExpireRun");
 
-}   
+}
 
 $file = File::Tail->new(name=>$READLOG, maxinterval=>10);
 
@@ -154,22 +154,22 @@ while (defined ($line=$file->read))
 {
    $NOW = time();
    $_ = $line;
-   
+
    if (/sshd\[\d+\]: Failed password for .* from (\d+\.\d+\.\d+\.\d+) port/)
    {
        my $ip = $1;
-       
+
        $ipcount{$ip}++;
-    
+
        blockHost($ip) if ($ipcount{$ip} > $THRESHOLD );
-   
-   }      
-   
+
+   }
+
    expireHosts() if ($NOW > $nextExpireRun);
-   
+
    dumpTable if ($DUMPTABLE);
-    
-}   
+
+}
 
 
 
