a3ef03f581897167ef8093f6b4788a0b9a745076
Bill Pemberton <wfp5p@virginia.edu>
2010-01-30 20:12:53 -0500

    delete/rename in preparation of making nukessh rpm

--- nukessh.pl
+++ nukessh.pl
@@ -1,53 +1,74 @@
-#! /uva/bin/perl
+#! /usr/bin/perl
 
-use lib '/common/uva/lib/toobp/lib/site_perl';
-use lib '/common/uva/lib/toobp/lib/site_perl/aix';
+# Linux version uses iptable and parses OpenSSH logs
+
+use strict;
 use POSIX;
+
 use File::Tail;
 use Socket;
-use NDBM_File;
+use GDBM_File;
 use Getopt::Long;
-use FileHandle;
+
+use Log::Dispatch;
+use Log::Dispatch::Screen;
 
 
-$READLOG='/etc/uva/tcp_wrapper/log';
-$LOGFILE='/etc/uva/nukessh/nukessh.log';
-$NUKEDBM='/etc/uva/nukessh/nukedbm';
-$EXPIRECYCLE = 3600;
-$EXPIRE=43200; # how long a host stays blocked
-$DECAY=10;
-$THRESHOLD=100; # 
+
+our ($DEBUG, $DEBUGOPT, $LOG, $NOW);
+our %DBM;
+our %ipcount;
+
+my $DUMPTABLE = 0;
+my $READLOG='/var/log/secure'; # This may be /var/log/messages for some systems
+my $LOGFILE='/etc/uva/nukessh/nukessh.log';
+my $NUKEDBM='/etc/uva/nukessh/nukedbm';
+my $EXPIRECYCLE = 3600;
+my $EXPIRE=43200; # how long a host stays blocked
+my $DECAY=10;
+my $THRESHOLD=100;
 
 GetOptions('debug',\$DEBUGOPT);
 
 $DEBUG=1 if ($DEBUGOPT);
 
 # run in background
-
-
-if ( $pid = fork())
-{
-   print "$pid\n";
-   exit 0;
-}
-
-
-setsid;
-
+## no critic
 
 open(STDIN,"</dev/null");
 open(STDOUT,">/dev/null");
 open(STDERR,">/dev/null");
 
-$LOG = new FileHandle ">> $LOGFILE";
-$LOG->autoflush(1);
+## use critic
 
-tie %DBM, "NDBM_File", $NUKEDBM, O_RDWR|O_CREAT, 0640
+exit 0 if (fork());
+setsid;
+
+# set up logging
+
+sub addTS # add a timestamp to the log entry
+{
+    my %p = @_;
+
+    return strftime("%b %e %H:%M:%S %Y ",localtime) . $p{message} . "\n";
+}
+
+
+$LOG = Log::Dispatch->new;
+$LOG->add(Log::Dispatch::File->new(name => 'logfile',
+				   min_level => 'debug',
+				   callbacks => \&addTS));
+
+
+## no critic
+
+tie %DBM, "GDBM_File", $NUKEDBM, O_RDWR|O_CREAT, 0640
    or die "Unable to open DBM database";
 
+## use critic
 
 
-$nextExpireRun = time + $EXPIRECYCLE;
+my $nextExpireRun = time + $EXPIRECYCLE;
 
 $SIG{USR1}=\&flip_debug;
 $SIG{USR2}=\&set_dump;
@@ -62,126 +83,124 @@ sub flip_debug
    $DEBUG = (! $DEBUG );
 }
 
-sub logIt 
+sub logIt
 {
   my ($message,$force) = @_;
-  
+
   return if ( (! $DEBUG) && ( ! $force) );
-  
-  my $now_string = strftime "%b %e %H:%M:%S %Y", localtime();
-  
-  print $LOG "$now_string $message\n";
+
+  $LOG->log(level=>'debug', message => $message);
+
 }
 
 sub blockHost
-{   
+{
     my $ip = shift;
-    
-    system "/usr/sbin/route add $ip 0.0.0.0 -blackhole";
-    
+
+    system "/sbin/iptables -A nukessh -s $ip -j DROP";
+
     $DBM{$ip} = $NOW + $EXPIRE;
-    
+
     delete $ipcount{$ip};
-    
+
     logIt("blocking $ip",1);
-    
-}  
+
+}
 
 sub unblockHost
-{   
+{
     my $ip = shift;
-    
-    system "/usr/sbin/route delete $ip 0.0.0.0";
-    
+
+    system "/sbin/iptables -D nukessh -s $ip -j DROP";
+
     delete $DBM{$ip};
-    
+
     logIt("unblocking $ip",1);
-    
-}  
+
+}
 
 sub dumpTable
-{ 
-   
+{
+
    my $saveDebug = $DEBUG;
    my $entry;
-   
+
    $DEBUG = 1;
-    
+
    logIt ("Dumping ipcounts table:");
-    
+
     while (my ($key,$val) = each %ipcount)
     {
 	logIt ("  $key $val");
     }
-   
+
    logIt ("Dumping DBM file:");
-   
+
    while  ( my ($key,$val) = each %DBM)
    {
       logIt ("  $key $val");
    }
-   
-   undef $DUMPTABLE;
-   $DEBUG = saveDebug;
-}   
 
-   
-  
+   $DUMPTABLE = 0;
+   $DEBUG = $saveDebug;
+}
+
+
+
 sub expireHosts
 {
    my $entry;
-   
+
    logIt("Doing expire.....");
-    
+
    while  ( my ($key,$val) = each %ipcount)
    {
        if ($val <= $DECAY) { delete $ipcount{$key}; }
        else { $ipcount{$key} -= $DECAY; }
-   }	
-   
+   }
+
    while  ( my ($key,$val) = each %DBM)
    {
        if ($DBM{$key} < $NOW)
        {
            unblockHost($key);
-       	   
+
        }
 
    }
-   
+
    $nextExpireRun = $NOW + $EXPIRECYCLE;
-   
+
    logIt("Expire done, next expire at $nextExpireRun");
 
-}   
+}
 
-$file = File::Tail->new(name=>$READLOG, maxinterval=>10);
+my $file = File::Tail->new(name=>$READLOG, maxinterval=>10);
 
 logIt ("nukessh started",1);
+
+my $line;
 
 while (defined ($line=$file->read))
 {
    $NOW = time();
    $_ = $line;
-   
-   if (/sshd2\[\d+\]: connection from "(\d+\.\d+\.\d+\.\d+)"/)
+
+   if (/sshd\[\d+\]: Failed password for .* from (\d+\.\d+\.\d+\.\d+) port/)
    {
        my $ip = $1;
 
-       if ( ! ($ip =~ /^128\.143\./) )
-       {
-	    $ipcount{$ip}++;
-    
-            blockHost($ip) if ($ipcount{$ip} > $THRESHOLD );
-       }	   
-   
-   }      
-   
+       $ipcount{$ip}++;
+
+       blockHost($ip) if ($ipcount{$ip} > $THRESHOLD );
+
+   }
+
    expireHosts() if ($NOW > $nextExpireRun);
-   
+
    dumpTable if ($DUMPTABLE);
-    
-}   
+
+}
 
 
 
