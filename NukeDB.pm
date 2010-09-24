81132edfebd5965b8b894487fa2b732272593cec
Bill Pemberton <wfp5p@virginia.edu>
2010-09-24 17:24:52 -0400

    Add NukeDB.pm
    This is module to do the DB stuff using SQLite

---
+++ NukeDB.pm
@@ -0,0 +1,85 @@
+package NukeDB;
+
+use strict;
+use Carp;
+use DBI;
+use Data::Alias;
+
+
+sub new
+{
+    my ($class, %arg) = @_;
+    my $this;
+
+    croak "database not defined!" if ( !defined $arg{DB} );
+
+    $this->{dbh} = DBI->connect("dbi:SQLite:dbname=$arg{DB}","","") or croak;
+
+    my @tables = $this->{dbh}->selectrow_array("select name from sqlite_master where name='nukessh' and type='table'");
+    if ($#tables == -1) {
+	$this->{dbh}->do("create table nukessh (ip text primary key,
+                                    expire integer default 0,
+                                    blocks integer default 0)");
+    }
+
+    bless $this, $class;
+    return $this;
+}
+
+
+sub insertexpire
+{
+    my $this = shift;
+    alias my $dbh = $this->{dbh};
+    my $ip = $dbh->quote(shift @_);
+    my $expire = shift;
+
+    my @row = $dbh->selectrow_array("select ip,expire from nukessh where ip=$ip");
+    if ($#row != -1) {
+	if ($row[1] < $expire) {
+	    $dbh->do("update nukessh set expire=$expire, blocks=blocks + 1 where ip=$ip");
+	}
+    } else {
+	$dbh->do("insert into nukessh VALUES ($ip, $expire,1)");
+    }
+}
+
+sub clearexpire
+{
+    my $this = shift;
+    alias my $dbh = $this->{dbh};
+    my $ip = $dbh->quote(shift @_);
+
+    $dbh->do("update nukessh set expire=0 where ip=$ip");
+}
+
+# return array of all ips with expire > 0 and <= now
+sub getexpires
+{
+    my $this = shift;
+    my $now = shift;
+    alias my $dbh = $this->{dbh};
+
+    my $x = $dbh->selectall_arrayref("select ip from nukessh where expire > 0 and expire <= $now");
+
+    my @x = map { $_->[0] } @$x;
+
+    return @x;
+}
+
+sub getactive
+{
+    my $this = shift;
+    my $now = shift;
+    alias my $dbh = $this->{dbh};
+
+    my $x = $dbh->selectall_arrayref("select ip from nukessh where expire != 0 and expire > $now");
+
+    my @x = map { $_->[0] } @$x;
+
+    return @x;
+}
+
+
+
+1;
